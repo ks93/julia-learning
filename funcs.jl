ff6765b36a96ed60fa89e93433b3f0e79c757432
Kelvin Sundli <kelvin.sundli@cognite.com>
2020-10-13 23:08:20 +0200

    element-wise symbol

--- funcs.jl
+++ funcs.jl
@@ -9,7 +9,7 @@ using StatsPlots
 data_url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
 
 col_names = ["sepal_length", "sepal_width", "petal_length", "petal_width", "class"]
-col_symbols = Symbol.(col_names)#[Symbol(cn) for cn in col_names]
+col_symbols = Symbol.(col_names)
 
 f = CSV.File(HTTP.get(data_url).body, header=false, ignoreemptylines=true)
 iris = DataFrame(f)
